d9d504ca6e532d6a3ef418edcfa3d67f33bf0b9b
“Biel007O010” <bielflaquegrive@gmail.com>
2026-02-07 19:59:55 +0100

    Hola

---
+++ myfirstprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int amplada = 7;
+    int llargada = 14;
+    int area;
+    cout << "Area d'un rectangle" << "\n";
+    area = amplada*llargada;
+    cout << "L'area del rectangle és de " << area << " cm" << "\n"; 
+    return 0;
+}
---
+++ secondprogram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y, suma;
+    cout << "Calculadora de sumas" << "\n";
+    cout << "Introdueix el primer nombre: ";
+    cin >> x;
+    cout << "Introdueix el segon nombre: ";
+    cin >> y;
+    suma = x+y;
+    cout << "El resultat és: " << suma << "\n";
+    return 0;
+
+}
