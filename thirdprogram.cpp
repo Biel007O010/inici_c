bc24b39caa603315bc25c68a354f6f75d87fe93b
“Biel007O010” <bielflaquegrive@gmail.com>
2026-02-08 19:06:33 +0100

    Tipus de dades

---
+++ thirdprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    float f1 = 35e3;
+    double d1 = 12E4;
+    std::cout << f1 << std::endl;
+    std::cout << d1 << std::endl;
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    std::cout << isCodingFun << std::endl; // Outputs 1 (true)
+    std::cout << isFishTasty << std::endl; // Outputs 0 (false)
+    char a = 65, b = 66, c = 67;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+}
